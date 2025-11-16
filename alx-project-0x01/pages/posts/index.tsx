d658d5e81323dd68681551c91699839c0fd9c6d1
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-11-16 22:46:42 +0300

    quiz 0 correction

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,13 @@
+// pages/posts/index.tsx
+import React from 'react';
+
+const PostsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Posts Page</h1>
+      <p>Welcome to the posts page!</p>
+    </div>
+  );
+};
+
+export default PostsPage;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,13 @@
+// pages/posts/index.tsx
+import React from 'react';
+
+const UsersPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      <p>Welcome to the users page!</p>
+    </div>
+  );
+};
+
+export default UsersPage;
